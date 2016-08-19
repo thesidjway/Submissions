7279b41cf34b5a40e3620157c4da257434ebd240
Siddharth Jha <siddharthjha@outlook.com>
2016-08-19 19:03:10 +0530

    Added trafficlights.py

--- trafficlights.py
+++ trafficlights.py
@@ -5,6 +5,7 @@ n,m=int(n),int(m)
 
 
 def nexthigher(flash_int,time_now):
+	i=0
 	while((time_now+i)%int(flash_int) != 0):
 		i+=1
 	return time_now+i
@@ -15,12 +16,12 @@ flash=[]
 flash=raw_input().split()
 
 reachTime=[100000 for i in range(n)]
-visited=[0 for i in range(n)]
-pending=[i for i in range(n)]
+pending=[i+1 for i in range(n)]
 
 adjMat = [[100000 for i in range(n)] for j in range(n)]
 adjList= [[] for i in range(n)] 
 i=0
+
 while i<m:
 	a,b,c=raw_input().split()
 	a,b,c=int(a),int(b),int(c)
@@ -30,6 +31,39 @@ while i<m:
 	adjList[b-1].append(a-1)
 	i+=1
 
-cost=0
 reachTime[source-1]=0
+
+while dest in pending:
+	#print pending
+	k=0
+	minval=100001
+	minindex=0
+	while k<len(reachTime):
+		#print reachTime
+		if minval>reachTime[k]:
+			if k+1 in pending:
+				minval=reachTime[k]
+				minindex=k
+		k+=1
+	#print "minval",minval
+	#print "minindex",minindex
+	pending.remove(minindex+1)
+	if minindex!=dest-1:
+	#	print "reachTime[minindex] before nexthigher",reachTime[minindex]
+	#	print "flash[minindex] before nexthigher",flash[minindex]
+		reachTime[minindex]=nexthigher(flash[minindex],reachTime[minindex])
+	#	print "reachTime[minindex] after nexthigher",reachTime[minindex]
+	#print "adjList[minindex]",adjList[minindex]
+	#print "adjMat[minindex]",adjMat[minindex]
+	for j in adjList[minindex]:
+		temp=reachTime[minindex]+adjMat[minindex][j]
+		if temp<reachTime[j]:
+			reachTime[j]=temp
+
+print reachTime[dest-1]
+
+
+
+
+
 
